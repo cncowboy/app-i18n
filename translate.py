ed960d3361fef37b8e50f7f0c942da5e0037118b
zhangxiaobing <zhangxiaobing@51yund.com>
2019-07-27 16:43:50 +0800

    支持web json

--- translate.py
+++ translate.py
@@ -203,11 +203,11 @@ class I18NTranslator:
                 value = ""
 
             # 拼接每一行的文本
-            str = "t{k}: '{v}'\n".format(k=int(key), v=value)
+            str = "t{k}: '{v}'".format(k=int(key), v=value)
             stringls.append(str)
 
         # 拼接字符串
-        text = "{\n" + ",".join(stringls) + "}\n"
+        text = "{\n" + ",\n".join(stringls) + "\n}\n"
         return text
 
 
